f244102b636d95f16bad1fbb752c4088408673ec
pduren <57735971+kParkz@users.noreply.github.com>
2022-10-19 23:47:24 -0500

    commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,30 +20,10 @@ public class PlayerMovement : MonoBehaviour
     // FixedUpdate called once a frame
     // Use FixedUpdate because Unity like it better for physics
     void FixedUpdate()
+    
     {
-
-
         MyInput();
         MovePlayer();
-        
-        // if (Input.GetKey("w")) {
-        //     rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
-        // }
-
-        // if (Input.GetKey("s")) {
-        //     rb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
-        // }
-
-        // // TODO: switch these to rotate the player instead of move sideways!
-
-        // if (Input.GetKey("a")) {
-        //     rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        // }
-
-        // if (Input.GetKey("d")) {
-        //     rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        // }
-
     }
 
     private void MyInput() {
