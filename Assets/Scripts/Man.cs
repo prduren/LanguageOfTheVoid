df1f500e2e7a2812e111cd368fccd2898d3ad726
pduren <57735971+kParkz@users.noreply.github.com>
2022-11-09 08:57:49 -0600

    level 1 mvp

--- Assets/Scripts/Man.cs
+++ Assets/Scripts/Man.cs
@@ -8,7 +8,9 @@ public class Man : MonoBehaviour
     GameObject Player;
     GameObject Man1;
     GameObject Man1Parent;
+    GameObject VoidHome;
     Vector3 originalPos;
+    float distanceFromVoidHome;
     bool manAttached = false;
 
     // Start is called before the first frame update
@@ -18,31 +20,39 @@ public class Man : MonoBehaviour
         Man1 = GameObject.Find("man1");
         originalPos = new Vector3(26, 0, -5);
         Man1Parent = GameObject.Find("man1Parent");
+        VoidHome = GameObject.Find("VoidHome");
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        distanceFromVoidHome = Vector3.Distance(Man1.transform.position, VoidHome.transform.position);
+        // Debug.Log(distanceFromVoidHome);
+
+        // grab the man
         if (Input.GetKeyDown(KeyCode.V)) {
             manAttached = true;
         }
 
-        // if (Input.GetKeyDown(KeyCode.C)) {
-               //  manAttached = false;
-        //}
 
+        // if grabbing man
         if (manAttached) {
             Man1.transform.SetParent(Player.transform);
             if (Input.GetKeyDown(KeyCode.C)) {
                 manAttached = false;
         }
+        // if not grabbing man / if let go of man
         }
         else if (!manAttached) {
                 Man1.transform.position = originalPos;
                 Man1.transform.SetParent(Man1Parent.transform);
         }
 
+        // if man is close enough to VoidHome then end level
+        if (distanceFromVoidHome < 3) {
+            Debug.Log("end level");
+        }
     }
 
 }
