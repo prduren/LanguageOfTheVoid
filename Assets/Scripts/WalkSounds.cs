86a065a635efb2c5af99ad15143d67e93301948b
pduren <57735971+kParkz@users.noreply.github.com>
2022-11-21 08:48:24 -0600

    walk sounds init

--- Assets/Scripts/WalkSounds.cs
+++ Assets/Scripts/WalkSounds.cs
@@ -6,7 +6,7 @@ public class WalkSounds : MonoBehaviour
 {
     
     GameObject Player;
-    GameObject[] woodFloor;
+    GameObject woodFloor;
     GameObject concreteFloor;
     AudioSource woodSoundSource;
     
@@ -14,21 +14,32 @@ public class WalkSounds : MonoBehaviour
     {
         Player = GameObject.Find("Player");
         woodSoundSource = GameObject.Find("woodStep").GetComponent<AudioSource>();
+        woodSoundSource.pitch = 2f;
         // Floor objects
-            woodFloor = GameObject.FindGameObjectsWithTag("woodFloor");
+            woodFloor = GameObject.FindGameObjectWithTag("woodFloor");
     }
 
-    void Update()
-    {
-        foreach (GameObject i in woodFloor) {
-            // if you're pretty much only on top of the thing
-            if  (((Player.transform.position.x - i.transform.position.x) < 1) & 
-                ((Player.transform.position.z - i.transform.position.z) < 1) &
-                ((Player.transform.position.x - i.transform.position.x) > -1) & 
-                ((Player.transform.position.z - i.transform.position.z) > -1)) {
-                    //woodSoundSource.PlayOneShot(woodSoundSource.clip);
-                    woodSoundSource.Play();
+    void Update() {
+        if  (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d")) {
+            if (!woodSoundSource.isPlaying) {
+                woodSoundSource.Play();
             }
         }
+        else {
+            woodSoundSource.Stop();
+        }
+
+        // this stuff below is for if you need to actually loop an array of each woodFloor with the woodFloor tag
+
+        // foreach (GameObject i in woodFloor) {
+            // if you're pretty much only on top of the thing
+            // if  (((Player.transform.position.x - i.transform.position.x) < 5) & 
+                // ((Player.transform.position.z - i.transform.position.z) < 5) &
+                //((Player.transform.position.x - i.transform.position.x) > -5) & 
+                // ((Player.transform.position.z - i.transform.position.z) > -5)) {
+                    //woodSoundSource.PlayOneShot(woodSoundSource.clip);
+                   // woodSoundSource.Play();
+            // }
+        //}
     }
 }
