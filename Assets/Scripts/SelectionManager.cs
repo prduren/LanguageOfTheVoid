7fbf62341273813dbc22fdec35de5fe54c1d1a65
pduren <57735971+kParkz@users.noreply.github.com>
2022-11-22 07:13:40 -0600

    Note modem init

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -7,6 +7,8 @@ public class SelectionManager : MonoBehaviour
     // Start is called before the first frame update
 
     GameObject Note;
+    public GameObject noteUI;
+    public static bool NoteIsOpen = false;
 
     void Start()
     {
@@ -19,10 +21,31 @@ public class SelectionManager : MonoBehaviour
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition); // this assumes the mouse is at the center of the screen
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Note") {
-            Debug.Log("hit");
+            //execute note opening logic
+            
+            if (Input.GetMouseButtonDown(0)) {
+                if (NoteIsOpen) {
+                    CloseNote();
+                } else {
+                    OpenNote();
+                }
+            }
         }
         else {
             Debug.Log("not hit");
         }
     }
+
+    public void CloseNote() {
+        noteUI.SetActive(false);
+        Time.timeScale = 1f;
+        NoteIsOpen = false;
+    }
+
+    public void OpenNote() {
+        noteUI.SetActive(true);
+        Time.timeScale = 0f;
+        NoteIsOpen = true;
+    }
+
 }
