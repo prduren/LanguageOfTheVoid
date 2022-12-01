84d08fe47ae0718b54781120cbbd07a113366d07
pduren <57735971+kParkz@users.noreply.github.com>
2022-12-01 15:08:20 -0600

    menu - spinning menu people

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,16 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+
+    GameObject[] MenuPeople;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MenuPeople = GameObject.FindGameObjectsWithTag("MenuPeople");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach(GameObject person in MenuPeople) {
+            person.transform.Rotate(0f, 0f, 6.0f * Time.deltaTime);
+        }
     }
 
     public void StartGame() {
