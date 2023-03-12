ca1045c871f0376e834a894b1e33f6ff8a062e09
pduren <57735971+kParkz@users.noreply.github.com>
2023-03-11 23:38:13 -0600

    L5 - thermometer, kotatsu, snowman, trees, snow

---
+++ Assets/TREEGEN 2023 - procedural tree generator/Plugins/TREEGENPlacement/Scripts/TREEGENPlacementSceneSettings.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+namespace TREEGEN.TREEGENPlacement
+{
+    //
+    // class TREEGENPlacementSceneSettings
+    //
+    public class TREEGENPlacementSceneSettings : MonoBehaviour
+    {
+        public GameObject parentForPrefabs;
+    }
+}
