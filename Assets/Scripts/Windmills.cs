84d4ef2d1de997c764b629694311985b4cfd288a
pduren <57735971+kParkz@users.noreply.github.com>
2023-05-27 15:00:36 -0500

    windmill L4 faster ref fix

--- Assets/Scripts/Windmills.cs
+++ Assets/Scripts/Windmills.cs
@@ -12,7 +12,7 @@ public class Windmills : MonoBehaviour
         windmills = GameObject.FindGameObjectsWithTag("windmill");
         foreach(GameObject wm in windmills) {
             Animator wmAnimator = wm.GetComponent<Animator>();
-            wmAnimator.speed = 20;
+            wmAnimator.speed = 40;
         }
     }
 
